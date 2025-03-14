1afe4023275d24f702741ae57503e95ef729982c
James Lyne <jim+github@not-null.co.uk>
2025-03-14 16:18:29 +0000

    Refactor gradle scripts
    - Switch to Kotlin
    - Switch to plugin-yml gradle plugin
    - Switch to using version catalog for dependencies
    - Use java toolchain

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+
+plugins {
+    java
+    alias(libs.plugins.pluginYmlPaper)
+}
+
+group = "me.twoleggedcat"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	maven {
+		url = uri("https://repo.papermc.io/repository/maven-public/")
+	}
+}
+
+dependencies {
+	compileOnly(libs.paperApi)
+}
+
+paper {
+    main = "me.twoleggedcat.bettercats.Main"
+    apiVersion = libs.versions.paperApi.get().replace(Regex("\\-R\\d.\\d-SNAPSHOT"), "")
+    authors = listOf("Jim (AnEnragedPigeon)", "TwoLeggedCat")
+    description = "Cat behaviour enhancements"
+}
+
+tasks {
+    compileJava {
+        dependsOn(clean)
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+        options.encoding = "UTF-8"
+    }
+}
